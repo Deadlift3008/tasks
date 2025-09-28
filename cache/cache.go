3d93620af98acf3560952eb6d7931dfd91ab6b80
deadlift3008 <deadlift3008@gmail.com>
2025-09-28 21:35:22 +0300

    add nemnogo huyni

--- cache/cache.go
+++ cache/cache.go
@@ -1,1 +1,83 @@
 package cache
+
+import "sync"
+
+type DB interface {
+	Get(key string) (string, error)
+}
+
+type CacheI interface {
+	Get(key string) (string, error)
+	KeysInCache() ([]string, error)
+	MGet(keys []string) ([]string, error)
+}
+
+type Cache struct {
+	mu   sync.Mutex
+	data map[string]string
+	db   DB
+}
+
+func (c *Cache) Get(key string) (string, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	value, exist := c.data[key]
+
+	if !exist {
+		dbValue, error := c.db.Get(key)
+
+		if error != nil {
+			return "", error
+		}
+
+		c.data[key] = dbValue
+
+		return dbValue, nil
+	}
+
+	return value, nil
+}
+
+func (c *Cache) MGet(keys []string) ([]string, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	values := make([]string, 0, len(keys))
+
+	for _, key := range keys {
+		valueFromCache, exist := c.data[key]
+
+		if exist {
+			values = append(values, valueFromCache)
+			continue
+		}
+
+		valueFromDb, err := c.db.Get(key)
+
+		if err != nil {
+			return nil, err
+		}
+
+		values = append(values, valueFromDb)
+	}
+
+	return values, nil
+}
+
+func (c *Cache) KeysInCache() ([]string, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	keys := make([]string, 0, len(c.data))
+
+	for k := range c.data {
+		keys = append(keys, k)
+	}
+
+	return keys, nil
+}
+
+func NewCache(db DB) Cache {
+	return Cache{
+		db:   db,
+		data: make(map[string]string),
+	}
+}
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,1 +1,115 @@
 package cache
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type Call struct {
+	method string
+	args   []string
+}
+
+type DBMock struct {
+	data  map[string]string
+	calls []Call
+}
+
+func (db *DBMock) Get(key string) (string, error) {
+	db.calls = append(db.calls, Call{
+		method: "Get",
+		args:   []string{key},
+	})
+
+	return db.data[key], nil
+}
+
+func (db *DBMock) Keys() ([]string, error) {
+	db.calls = append(db.calls, Call{
+		method: "MGet",
+	})
+
+	keys := make([]string, 0, len(db.data))
+
+	for k := range db.data {
+		keys = append(keys, k)
+	}
+
+	return keys, nil
+}
+
+func (db *DBMock) GetCalls() []Call {
+	return db.calls
+}
+
+func (db *DBMock) ClearCalls() {
+	db.calls = []Call{}
+}
+
+func NewDBMock(mockData map[string]string) DBMock {
+	return DBMock{
+		data: mockData,
+	}
+}
+
+func TestCache(t *testing.T) {
+	testData := map[string]string{
+		"key123":  "value123",
+		"zhopa":   "zhopaValue",
+		"hui":     "huiValue",
+		"gavno":   "gavnoValue",
+		"muravei": "muraveiValue",
+	}
+
+	t.Run("Should get value from db if no in cache", func(t *testing.T) {
+		dbMockInstance := NewDBMock(testData)
+		cache := NewCache(&dbMockInstance)
+
+		cache.Get("zhopa")
+		value, _ := cache.Get("zhopa")
+		calls := dbMockInstance.GetCalls()
+
+		require.Equal(t, value, "zhopaValue")
+		require.Equal(t, calls, []Call{{method: "Get", args: []string{"zhopa"}}})
+	})
+
+	t.Run("Should go in db only one time and then take from cache", func(t *testing.T) {
+		dbMockInstance := NewDBMock(testData)
+		cache := NewCache(&dbMockInstance)
+
+		for i := 0; i < 10; i++ {
+			cache.Get("zhopa")
+		}
+
+		calls := dbMockInstance.GetCalls()
+
+		require.Equal(t, len(calls), 1)
+		require.Equal(t, calls, []Call{
+			{
+				method: "Get",
+				args:   []string{"zhopa"},
+			},
+		})
+	})
+
+	t.Run("Should go in db in MGet if it is absents in cache", func(t *testing.T) {
+		dbMockInstance := NewDBMock(testData)
+		cache := NewCache(&dbMockInstance)
+
+		cache.Get("hui")
+		cache.Get("gavno")
+
+		dbMockInstance.ClearCalls()
+
+		result, _ := cache.MGet([]string{"hui", "gavno", "muravei"})
+
+		calls := dbMockInstance.GetCalls()
+
+		require.Equal(t, result, []string{"huiValue", "gavnoValue", "muraveiValue"})
+		require.Equal(t, calls, []Call{{
+			method: "Get",
+			args:   []string{"muravei"},
+		}})
+	})
+}
